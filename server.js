ad5f0dc4f0fa2b97d98bfb556dffc510367b7add
Cxlddy <rcds.ciampi@gmail.com>
2026-07-07 10:15:02 -0300

    feat(backend): preparar api para entrega em producao

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import 'dotenv/config';
+﻿import 'dotenv/config';
 import app from './src/app.js';
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.info(`Servidor rodando na porta ${PORT}`);
 });
